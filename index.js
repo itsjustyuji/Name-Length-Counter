4d9399c1c36a240199181b60bab3d27fdf493062
itsjustyuji <vedantsharmavs2002@gmail.com>
2025-08-13 16:45:27 +0530

    small changes

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get("/",(req,res)=>{
   res.render("index.ejs");
 })
 
+app.use(express.urlencoded({extended: true}));
+
+app.post("/submit",(req,res)=>{
+  const countofLetters=req.body["fName"].length+req.body["lName"].length;
+  res.render("index.ejs", {totalCount: countofLetters});
+});
+
 app.listen(port, ()=>{
   console.log(`Port ${port} is working.`);
-})
+});
